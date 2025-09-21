b34e548a944d183483e0dc25ae6f9625e30664df
ak <khaxar@gmail.com>
2025-09-21 20:09:56 +0330

    bronze->silver etl from raw parquet

---
+++ etl/bronze_to_silver.py
@@ -0,0 +1,27 @@
+import json, os
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, unix_timestamp
+
+
+with open("config/config.json") as f:
+    cfg = json.load(f)
+
+spark = SparkSession.builder.appName("BronzeToSilver").getOrCreate()
+
+raw_parquet = os.path.join(cfg["local"]["raw_path"], \
+                           "yellow_tripdata_2025-01.parquet")
+silver_out = os.path.join(cfg["local"]["silver_path"], \
+                                       "trips_silver.parquet")
+
+df = spark.read.parquet(raw_parquet)\
+    .withColumn("pickup_datetime", col("tpep_pickup_datetime").cast("timestamp"))\
+    .withColumn("dropoff_datetime", col("tpep_dropoff_datetime").cast("timestamp"))\
+    .withColumn("trip_duration_min",\
+                (unix_timestamp("dropoff_datetime") - unix_timestamp("pickup_datetime"))/60)\
+    .withColumnRenamed("PULocationID", "pu_location_id")\
+    .withColumnRenamed("DOLocationID", "do_location_id")\
+    .filter((col("trip_duration_min") > 1) & (col("trip_duration_min") < 180)\
+          & (col("passenger_count") > 0) & (col("fare_amount") > 0))
+
+df.write.mode("overwrite").parquet(silver_out)
+spark.stop()
