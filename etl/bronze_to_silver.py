8c96ec75920233a9da086b978c2c4119d2a9cb99
ak <khaxar@gmail.com>
2025-09-25 18:16:46 +0330

    support aws s3 integration in bronze->silver step

--- etl/bronze_to_silver.py
+++ etl/bronze_to_silver.py
@@ -3,8 +3,6 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, unix_timestamp
 from etl.logging_utils import get_logger
 
-logger = get_logger()
-
 BASE_DIR = \
 os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 CONF_PATH= \
@@ -12,29 +10,49 @@ os.path.join(BASE_DIR, "config", "config.json")
 
 with open(CONF_PATH) as f:
     cfg = json.load(f)
+mode = cfg["mode"]
 
-spark = SparkSession.builder.appName("BronzeToSilver").getOrCreate()
+if mode == "AWS":
+    import boto3
+    s3 = boto3.client(
+        "s3",
+        region_name=cfg["aws"]["region"]
+        )
+    bucket = cfg["aws"]["s3_bucket"]
+    raw_key = "dev/projects/nyc_taxi_etl/raw/yellow_tripdata_2025-01.parquet"
+    local_parquet = os.path.join(BASE_DIR, "tmp/yellow.parquet")
 
-raw_parquet = os.path.join(BASE_DIR, cfg["local"]["raw_path"], \
-                           "yellow_tripdata_2025-01.parquet")
-silver_out = os.path.join(BASE_DIR, cfg["local"]["silver_path"], \
-                                       "trips_silver.parquet")
+    s3.download_file(bucket, raw_key, local_parquet)
+    raw_parquet = local_parquet
+
+    silver_out = f"{bucket}/dev/projects/nyc_taxi_etl/silver/trips_silver.parquet"
+
+elif mode == "LOCAL":
+
+    logger = get_logger()
+
+    spark = SparkSession.builder.appName("BronzeToSilver").getOrCreate()
+
+    raw_parquet = os.path.join(BASE_DIR, cfg["local"]["raw_path"], \
+                            "yellow_tripdata_2025-01.parquet")
+    silver_out = os.path.join(BASE_DIR, cfg["local"]["silver_path"], \
+                                        "trips_silver.parquet")
 
 
-logger.info("reading raw parquet %s", raw_parquet)
+    logger.info("reading raw parquet %s", raw_parquet)
 
-df = spark.read.parquet(raw_parquet)\
-    .withColumn("pickup_datetime", col("tpep_pickup_datetime").cast("timestamp"))\
-    .withColumn("dropoff_datetime", col("tpep_dropoff_datetime").cast("timestamp"))\
-    .withColumn("trip_duration_min",\
-                (unix_timestamp("dropoff_datetime") - unix_timestamp("pickup_datetime"))/60)\
-    .withColumnRenamed("PULocationID", "pu_location_id")\
-    .withColumnRenamed("DOLocationID", "do_location_id")\
-    .filter((col("trip_duration_min") > 1) & (col("trip_duration_min") < 180)\
-          & (col("passenger_count") > 0) & (col("fare_amount") > 0))
+    df = spark.read.parquet(raw_parquet)\
+        .withColumn("pickup_datetime", col("tpep_pickup_datetime").cast("timestamp"))\
+        .withColumn("dropoff_datetime", col("tpep_dropoff_datetime").cast("timestamp"))\
+        .withColumn("trip_duration_min",\
+                    (unix_timestamp("dropoff_datetime") - unix_timestamp("pickup_datetime"))/60)\
+        .withColumnRenamed("PULocationID", "pu_location_id")\
+        .withColumnRenamed("DOLocationID", "do_location_id")\
+        .filter((col("trip_duration_min") > 1) & (col("trip_duration_min") < 180)\
+            & (col("passenger_count") > 0) & (col("fare_amount") > 0))
 
-df.write.mode("overwrite").parquet(silver_out)
+    df.write.mode("overwrite").parquet(silver_out)
 
-logger.info("silver written to %s", silver_out)
+    logger.info("silver written to %s", silver_out)
 
-spark.stop()
+    spark.stop()
